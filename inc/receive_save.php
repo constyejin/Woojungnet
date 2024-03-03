723f119c9ca9f0383c767af2d1088630719f03e7
constyejin <lightup.jin@gmail.com>
2024-03-03 20:08:21 +0900

    🔥Delete all files

--- inc/receive_save.php
+++
@@ -1,9 +0,0 @@
-<?
-session_start();
-include $_SERVER["DOCUMENT_ROOT"]."/lib/func.php"; 
-dbconn();
-
-$query="insert into receive set conten='".$wc_idx."', regdate=now() ";
-mysql_query($query);
-
-?>
