6665fda9fef3b885feddae87636ba190b76db2c7
constyejin <lightup.jin@gmail.com>
2024-03-16 23:39:52 +0900

    🚧 slick slide in progress

--- 경기보링공업/240313/script/car-slide.js
+++ 경기보링공업/240313/script/car-slide.js
@@ -7,23 +7,67 @@ $(document).ready(function(){
                   <i class="fa-solid fa-angle-right"></i>
                 </button>`,       
     dots: true,
-    autoplay: false
+    autoplay: false,
   });
 
-  $('.slide-img-list').find('[data-slick-index=0]').children('li').animate({
-    opacity: 1
+  $('.slide-thum-list').slick({
+    prevArrow: false,         
+    nextArrow: false,   
+    slidesToShow: 10, 
+    focusOnSelect: true,
+    centerMode: false,
+    infinite: false,
   });
 
-  $(".slide-img-list").on("afterChange", function(slick,aaa){
-    $(this).find(".slick-slide").children("li").css({
-      opacity: 0,
-    });
+  let cur = 0;
+  let len = $('.slick-track li').length;
+  $('.slick-track').width(120 * len);
 
-    let c = aaa.currentSlide;
-    $(this).find("[data-slick-index="+c+"]").children("li").animate({
-      opacity: 1,
-    });
-  });
+  function sliding(dir,num){
+    cur = cur + dir;
+    if(len <= num) {
+      // 아이템이 10개 미만인 경우
+      $('.prev-btn').attr('disabled', true);
+      $('.next-btn').attr('disabled', true);
+      return;
+    }
+
+    if(cur >= len - num) {
+      // 마지막 페이지일 떄
+      cur = len - num;
+      $('.prev-btn').attr('disabled', false);
+      $('.next-btn').attr('disabled', true);
+    } else if(cur <= 0) {
+      // 첫 번째 페이지일 때
+      cur = 0;
+      $('.prev-btn').attr('disabled', true);
+      $('.next-btn').attr('disabled', false);
+    } else {
+      $('.prev-btn').attr('disabled', false);
+      $('.next-btn').attr('disabled', false);
+    }
+
+    $('.slick-track').animate({
+      marginLeft : -120 * cur + "px"
+    })
+
+    $('.slick-track').animate({
+      marginLeft : -120 * cur + "px"
+    })
+  }
+
+$('.prev-btn').on('click', function(){
+  sliding(-10, 10);
 })
 
+$('.next-btn').on('click', function(){
+  sliding(10, 10);
+})
+
+    // 썸네일 클릭시 해당 슬라이드 보이기
+    // $('.slide-thum-item').on('click', function(){
+    //   var index = $(this).index();
+    //   $('.slide-img-list').slick('slickGoTo', index);
+    // });
+})
 
