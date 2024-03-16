264ca1c9dfc55bc47adbf5535289e1eeb9246bb5
constyejin <lightup.jin@gmail.com>
2024-03-17 01:20:55 +0900

    🚧 작업현황 뷰페이지 슬라이드 작업중

--- 경기보링공업/240313/script/car-slide.js
+++ 경기보링공업/240313/script/car-slide.js
@@ -1,73 +1,76 @@
-$(document).ready(function(){
-  $('.slide-img-list').slick({
-    prevArrow: `<button type="button" class="slick-prev">
-                  <i class="fa-solid fa-angle-left"></i>
-                </button>`,         
-    nextArrow: `<button type="button" class="slick-next">
-                  <i class="fa-solid fa-angle-right"></i>
-                </button>`,       
-    dots: true,
-    autoplay: false,
-  });
+// swiper
+let cur = 0;
 
-  $('.slide-thum-list').slick({
-    prevArrow: false,         
-    nextArrow: false,   
-    slidesToShow: 10, 
-    focusOnSelect: true,
-    centerMode: false,
-    infinite: false,
-  });
-
-  let cur = 0;
-  let len = $('.slick-track li').length;
-  $('.slick-track').width(120 * len);
-
-  function sliding(dir,num){
-    cur = cur + dir;
-    if(len <= num) {
-      // 아이템이 10개 미만인 경우
-      $('.prev-btn').attr('disabled', true);
-      $('.next-btn').attr('disabled', true);
-      return;
-    }
-
-    if(cur >= len - num) {
-      // 마지막 페이지일 떄
-      cur = len - num;
-      $('.prev-btn').attr('disabled', false);
-      $('.next-btn').attr('disabled', true);
-    } else if(cur <= 0) {
-      // 첫 번째 페이지일 때
-      cur = 0;
-      $('.prev-btn').attr('disabled', true);
-      $('.next-btn').attr('disabled', false);
-    } else {
-      $('.prev-btn').attr('disabled', false);
-      $('.next-btn').attr('disabled', false);
-    }
-
-    $('.slick-track').animate({
-      marginLeft : -120 * cur + "px"
-    })
-
-    $('.slick-track').animate({
-      marginLeft : -120 * cur + "px"
-    })
+let len = $('.work-status .img-list > ul > li').length;
+console.log(len)
+$('.work-status .img-list > ul').width(120 * len);
+    
+function sliding(dir,num){
+  cur = cur + dir;
+  if(len <= num) {
+    // 아이템이 10개 미만인 경우
+    $('.prev-btn').attr('disabled', true);
+    $('.next-btn').attr('disabled', true);
+    return;
+  }
+  if(cur >= len - num) {
+    // 마지막 페이지일 떄
+    cur = len - num;
+    $('.prev-btn').attr('disabled', false);
+    $('.next-btn').attr('disabled', true);
+  } else if(cur <= 0) {
+    // 첫 번째 페이지일 때
+    cur = 0;
+    $('.prev-btn').attr('disabled', true);
+    $('.next-btn').attr('disabled', false);
+  } else {
+    $('.prev-btn').attr('disabled', false);
+    $('.next-btn').attr('disabled', false);
   }
 
-$('.prev-btn').on('click', function(){
+  $('.work-status .img-list > ul').animate({
+    marginLeft : -120 * cur + "px"
+  })
+
+  $('.work-status .img-list > ul').animate({
+    marginLeft : -120 * cur + "px"
+  })
+}
+
+$('.work-status .img-list .prev-btn').on('click', function(){
   sliding(-10, 10);
 })
 
-$('.next-btn').on('click', function(){
+$('.work-status .img-list .next-btn').on('click', function(){
   sliding(10, 10);
 })
 
-    // 썸네일 클릭시 해당 슬라이드 보이기
-    // $('.slide-thum-item').on('click', function(){
-    //   var index = $(this).index();
-    //   $('.slide-img-list').slick('slickGoTo', index);
-    // });
+// 차량이미지 swipe기능
+$(function(){
+  var bx = $('.bxslider').bxSlider({
+    mode: 'fade',
+    speed: 100,
+    slideWidth: 1200,
+    pagerType: 'short',
+    nextText: '',
+    prevText: '',
+    pager: true,
+    infiniteLoop: false,
+    onSlideAfter: function($slideElement, oldIndex, newIndex){
+      console.log('bx', newIndex);
+      $('.view-thumb .img-list > ul > li[data-thumb="'+(newIndex+1)+'"]').addClass('active').siblings().removeClass('active');
+
+      $('.view-thumb .img-list > ul').animate({
+        marginLeft: -(Math.floor(newIndex / 10) * 1200) + "px"
+      });
+    }
+  });
+  
+  $('.view-thumb .img-list > ul > li').on('mouseenter',function(e){
+    var target = $(this).data('thumb');
+    $(this).addClass('active').siblings().removeClass('active');
+    console.log(target);
+    bx.goToSlide(target-1);
+  });
 })
 
