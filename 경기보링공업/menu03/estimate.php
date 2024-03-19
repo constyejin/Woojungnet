e26b5458f296c75ea81cafbd67ac257dd0f42d37
constyejin <lightup.jin@gmail.com>
2024-03-19 17:28:55 +0900

    📝 테이블에 스팸방지 칸 추가

--- 경기보링공업/menu03/estimate.php
+++ 경기보링공업/menu03/estimate.php
@@ -7,12 +7,16 @@
 <main class="estimate">
   <section>
     <h2 class="sub-title">견적신청</h2>
+    <div class="sub-txt">
+      <p>아래 내용을 남겨주시면 신속하게 보링전문상담원이 연락을 드립니다.</p>
+    </div>
+
     <form name="" method="" action="" enctype="">
       <table>
         <thead>
           <tr>
             <th>이름</th>
-            <td><div class="input-wrap"><input type="text"></div></td>
+            <td><input type="text"></td>
           </tr>
           <tr>
             <th>연락처</th>
@@ -97,40 +101,32 @@
             </td>
           </tr>
 
+          <tr class="spam"> 
+            <th>스팸방지</th>
+            <td class="sm-input">
+              <input type="text" value="345533" disabled>
+              <input type="text">
+              <span>좌측 숫자를 입력하여주세요.</span>
+            </td>
+          </tr>
+
           <tr>
             <td style="border:none;"></td>
-          </tr>
-
-          <tr class="upload-img">
-            <td style="padding:0; border:none; font-weight: 700;">등록파일: 
-              <span>0</span> / 60개
-            </td>
-
-            <td align="right" style="padding:8px 0; border:none;">
-              <button class="file-btn">파일찾기</button>
-              <button class="reset-btn">초기화</button>
-            </td>
-          </tr>
-
-          <tr>
-            <td colspan="2" style="padding:0;border:none;">
-              <div style="width:1200px; height:110px; overflow-y:scroll; padding:10px; border:1px solid #cccccc; margin-bottom:5px;"></div>
-            </td>
           </tr>
         </tbody>
       </table>
 
       <div class="personal-info">
-        <b>개인정보처리방침</b>
+        <b>개인정보수집</b>
         <div class="personal-info-txt">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam alias eos, facilis veniam, doloremque, architecto animi reiciendis deserunt atque accusamus quod in dolore veritatis necessitatibus labore hic dicta neque repellat!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse asperiores, magni architecto optio id aliquid est exercitationem ea quos, quis molestiae fuga eum illum consectetur labore accusantium? Sed, beatae maiores.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde eaque repellendus porro fugiat maiores libero quos, impedit distinctio itaque vel velit sed? Totam earum nobis amet aliquam minima? Unde, ut.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non tempore delectus molestias enim nostrum ullam quibusdam, officia voluptates impedit quae iusto sequi. Aliquam natus odit eligendi dolores pariatur quam architecto?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam alias eos, facilis veniam, doloremque, architecto animi reiciendis deserunt atque accusamus quod in dolore veritatis necessitatibus labore hic dicta neque repellat!
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse asperiores, magni architecto optio id aliquid est exercitationem ea quos, quis molestiae fuga eum illum consectetur labore accusantium? Sed, beatae maiores.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde eaque repellendus porro fugiat maiores libero quos, impedit distinctio itaque vel velit sed? Totam earum nobis amet aliquam minima? Unde, ut.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non tempore delectus molestias enim nostrum ullam quibusdam, officia voluptates impedit quae iusto sequi. Aliquam natus odit eligendi dolores pariatur quam architecto?
+          <p>당사는 원활한 고객상담,각종서비스의 제공을 위해 아래와 같은 개인정보를 수집하고 있습니다</p>
+          <p><b>[가]수집하는 개인정보의 항목</b> : 이름, 연락처 ,이메일, 차량명, 년식</p>
+          <p><b>[나]수집하는 개인정보의 수집방법</b> : 자발적으로 온라인상에서 기재하는 방법으로 수집합니다</p>
+          <p><b>정보공개</b> :법령에 의한것이 아닌 경우를 제외하고 일체의 외부공개나 위탁을 하지 않습니다.</p>
+          <p><b>보유기간</b> : 원칙적으로, 개인정보 수집 및 이용목적이 달성된 후에는 해당 정보를 지체 없이 파기합니다</p>
+          <p>단, 작업현황에 광고용으로 등재하는 경우 차량명과 년식만 노출합니다</p>
+          <p><b>보존기간</b> : 계약 또는 청약철회 등에 관한 기록: 5년(전자상거래등에서의 소비자보호에 관한 법률)</p>
+          <p><b>대금결제 및 재화 등의 공급에 관한 기록</b> : 5년(전자상거래등에서의 소비자보호에 관한 법률)</p>
         </div>
         <div class="personal-info-check">
           <input type="checkbox">
