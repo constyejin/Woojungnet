2788562d5ce67ce1f005c929ff93dfc72c3bec78
constyejin <lightup.jin@gmail.com>
2024-03-20 00:15:55 +0900

    ✨ login / logout function

--- 경기보링공업/inc/login.php
+++ 경기보링공업/inc/login.php
@@ -6,13 +6,15 @@
     <div>
       <h2>로그인 <span>Login</span></h2>
       <p class="login-sub-txt">관리자전용 로그인페이지 입니다.</p>
-      <form name="" method="post" action="" target="" >
+      <form name="wform" method="post" enctype="multipart/form-data" target="HiddenFrm" onsubmit="login_check();">
+        <input type="hidden" name="idx" value="<?=$idx?>">
+        <input type="hidden" name="p_url" value="<?=$_SERVER['HTTP_REFERER'] ?>">
         <div>
           <div class="input-wrap">
-            <input type="text" name="userId" placeholder="아이디를 입력하세요">
+            <input type="text" name="login_id" placeholder="아이디를 입력하세요">
           </div>
           <div class="input-wrap">
-            <input type="password" name="userPw" placeholder="비밀번호를 입력하세요">
+            <input type="password" name="login_pass" placeholder="비밀번호를 입력하세요">
           </div>
           <button class="btn-blue-lg" type="submit">로그인</button>
         </div>
