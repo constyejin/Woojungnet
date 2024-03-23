5aacf939a371cc734c9392924c350b752a13815f
constyejin <lightup.jin@gmail.com>
2024-03-23 15:24:10 +0900

    ✨ login check function

--- 경기보링공업/inc/script.js
+++ 경기보링공업/inc/script.js
@@ -30,7 +30,7 @@ function c_ch2(num){
 function board_save(){
 	f=document.wform;
 	submitContents();
-	f.submit();
+	return true;
 }
 
 function all_del(){
@@ -50,5 +50,65 @@ function all_del(){
 
 	if(confirm('선택삭제 하시겠습니까?')){
 		document.lform.submit();
+	}
+}
+
+  function out_submit(){
+    f=document.outForm;
+      f.action="workStatus_save.php";
+      submitContents();
+      return true;
+  }
+
+function comma(obj) { 
+
+		var num = obj.value;
+        if (obj.value.length >= 4) {
+            re = /^$|,/g; 
+            num = num.replace(re, ""); 
+            fl="" 
+        if(isNaN(num)) { alert("문자는 사용할 수 없습니다.");return 0} 
+        if(num==0) return num 
+        if(num<0){ 
+            num=num*(-1) 
+            fl="-" 
+        }
+        else{ 
+            num=num*1 //처음 입력값이 0부터 시작할때 이것을 제거한다. 
+        } 
+            num = new String(num) 
+            temp="" 
+            co=3 
+            num_len=num.length 
+
+    while (num_len > 0){ 
+        num_len = num_len - co 
+        if(num_len<0){co=num_len+co;num_len=0} 
+			 temp=","+num.substr(num_len,co)+temp 
+        } 
+		obj.value =  fl+temp.substr(1);	
+    }
+} 
+
+function estimate_submit(){
+	f=document.con_form;
+	if(f.agree_1.checked==false){
+		alert('개인정보 수집에 동의해 주세요.');
+		return false;
+	}else if(f.spam_code.value!=f.spam_input.value){
+		alert('스팸방지 숫자를 확인해 주세요.');
+		f.spam_input.focus();
+		return false;
+	}else if(!f.con_name.value){
+		alert('이름을 입력해 주세요.');
+		f.con_name.focus();
+		return false;
+	}else if(!f.con_phone.value){
+		alert('연락처를 입력해 주세요.');
+		f.con_phone.focus();
+		return false;
+	}else{
+		submitContents();
+		return true;
 	}
 }
