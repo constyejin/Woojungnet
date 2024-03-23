28a91ccdef6ba5d9da454b7171ed6f9b53b65f2a
constyejin <lightup.jin@gmail.com>
2024-03-23 15:32:19 +0900

    📝 테이블 태그 수정

--- 경기보링공업/menu02/workStatus_write.php
+++ 경기보링공업/menu02/workStatus_write.php
@@ -1,8 +1,77 @@
 <? include $_SERVER['DOCUMENT_ROOT']."/inc/header.php"; ?>
 <? include $_SERVER['DOCUMENT_ROOT']."/inc/sub-visual.php"; ?>
+<?
+  if(!$_SESSION[login_level]||$_SESSION[login_level]>"40"){
+	  alert("권한이 없습니다.","/");
+	  exit;
+  }
+  if($wc_idx){
+    $qry = "select * from woojung_part where wc_idx = '$wc_idx'  ";
+    $row = mysql_fetch_array(mysql_query($qry));
+	$mode = 'modify';
+  }else{
+	  $mode = 'regist';
+  }
+?>
 <link rel="stylesheet" href="/inc/styles/sub-visual.css">
 <link rel="stylesheet" href="/inc/styles/form-table.css">
-<link rel="stylesheet" href="/menu02/style//workStatus.css">
+<link rel="stylesheet" href="/menu02/style/workStatus.css">
+<script language="JavaScript" type="text/JavaScript">
+  var upfile_num=1;
+  var num=0;
+  var img_count=0;
+
+  function fileInfo(f){
+    var file = f.files; // files 를 사용하면 파일의 정보를 알 수 있음
+    // 파일의 갯수만큼 반복
+    for(var i=0; i<file.length; i++){
+      if(img_count==60) break;  
+      
+      var reader = new FileReader(); // FileReader 객체 사용
+      reader.onload = function(rst){
+        if(img_count<60){
+        $('#img_box').append('<img src="' + rst.target.result + '"width="125" height="90" id="num'+num+'" data-image="tmp'+num+'" class="imgs"><input type=hidden name="tmpfile[]" value="'+ num +'"  id="tmp'+num+'" class="imgnames">'); // append 메소드를 사용해서 이미지 추가
+        // 이미지는 base64 문자열로 추가
+        // 이 방법을 응용하면 선택한 이미지를 미리보기 할 수 있음
+        num++; img_count++; 
+        document.getElementById("img_count").innerHTML=img_count;
+        }
+      }
+      reader.readAsDataURL(file[i]); // 파일을 읽는다
+    }
+  upfile_num++;
+  }
+  
+  $(document).on("click","#img_box .imgs",function(){
+    $(this).remove();
+    dataimg = $(this).data('image');
+    $("#"+dataimg).remove();
+    img_count--;
+    document.getElementById("img_count").innerHTML=img_count;
+  }); 
+
+  $(document).on("click","#img_del",function(){
+    $(".imgs").remove();
+    $(".imgnames").remove();
+    img_count=0;
+      document.getElementById("img_count").innerHTML=0;
+  }); 
+
+  function file_click(){
+    document.getElementById('uf'+upfile_num).click();
+  }
+
+function image_alldel(){
+	if(confirm("선택한 사진을 삭제하겠습니까?")){
+		f=document.outForm;
+		f.target="HiddenFrm";
+		f.action="img_alldel.php";
+		f.submit();
+		f.target="";
+		f.action="";
+	}
+}
+</script>
 
 <main class="work-status">
   <section>
@@ -12,27 +81,29 @@
     <p class="top-list-btn">
       <a href="/menu02/workStatus_list.php">목록보기 LIST</a>
     </p>
-
-    <form name="" method="" action="" enctype="">
+    
+    <form name='outForm' method='post' enctype="multipart/form-data" onsubmit="out_submit()">
+	<input type="hidden" name="mode" id="mode" value="<?=$mode?>">
+	<input type="hidden" name="wc_idx" value="<?=$wc_idx?>">
       <div class="table-form">
         <ul class="table-list border-top">
           <li class="table-title">차량명</li>
           <li class="table-content">
-            <input type="text" name="" value="">
+            <input type="text" name="wc_mem_etc" value="<?=$row[wc_mem_etc]?>">
           </li>
         </ul>
 
         <ul class="table-list">
           <li class="table-title">년식</li>
           <li class="table-content sm-input">
-            <input type="text" name="" value="">
+            <input type="text" name="wc_age" value="<?=$row[wc_age]?>">
           </li>
         </ul>
 
         <ul class="table-list">
           <li class="table-title">주행거리</li>
           <li class="table-content sm-input">
-            <input type="text" name="" value="">
+            <input type="text" name="carmile" value="<?=number($row[wc_mileage])?>" onKeyup="javascript:comma(this);">
             <span>Km</span>
           </li>
         </ul>
@@ -40,7 +111,7 @@
         <ul class="table-list">
           <li class="table-title">작업비</li>
           <li class="table-content sm-input">
-            <input type="text" name="" value="">
+            <input type="text" name="wc_keep_tel1" value="<?=number($row[wc_keep_tel1])?>" onKeyup="javascript:comma(this);">
             <span>원</span>
           </li>
         </ul>
@@ -49,12 +120,12 @@
           <li class="table-title">상세설명</li>
 
           <li class="table-content sm-only">
-            <textarea name="" id="" cols="30" rows="10" style="width:100%; height:220px;"></textarea>
+            <textarea name="carOptionadd" id="" cols="30" rows="10" style="width:100%; height:220px;"></textarea>
           </li>
 
           <li class="table-content lg-only">
             <script type="text/javascript" src="/inc/smarteditor2/js/HuskyEZCreator.js" charset="utf-8"></script>
-            <textarea name="carOptionadd" id="ir1" rows="10" cols="100" style="width:100%; height:320px; display:none;"></textarea>
+            <textarea name="carOptionadd" id="ir1" rows="10" cols="100" style="width:100%; height:320px; display:none;"><?=$row[wc_option_add]?></textarea>
             <script type="text/javascript">
               var oEditors = [];
               // 추가 글꼴 목록
@@ -107,41 +178,70 @@
 
         <ul class="upload-img">
           <li>등록파일: 
-            <span>0</span> / 60개
+            <span id="img_count">0</span> / 60개
           </li>
           <li class="table-list-btn">
-            <button class="file-btn">파일찾기</button>
+            <button class="file-btn" type="button" onclick="file_click();">파일찾기</button>
             <button class="reset-btn">초기화</button>
+			  <? for($i=1;$i<=10;$i++){ ?>
+			  <input type="file" accept="image/*" multiple="multiple" onchange="fileInfo(this)" name="upfile[]" id="uf<?=$i?>" style="display:none;"/>
+			  <? } ?>
           </li>
         </ul>
        
         <ul>
-          <li class="register-img-list">
-            <div class="register-img-item">
+          <li class="register-img-list" id="img_box">
+            <!--div class="register-img-item">
               <img src="/inc/assets/images/slide01.jpeg" alt="">
-            </div>
+            </div-->
           </li>
         </ul>
 
+<? if($wc_idx){ ?>
         <ul>
           <div class="existing-img-list">
+			<?
+			$imgCnt = 0;
+			for($i=1; $i<=60; $i++) {
+
+			  $fim="wc_img_".$i;
+			  $fileName = $row[$fim];
+			  $real_name = explode('/', $fileName);	
+			  
+			  if(strlen($real_name[0]) == 0)
+			  {
+				$fileName = '';
+				break;
+			  }
+			  else
+			  {
+				$imgCnt++;
+				$fileName = $site_u[home_url]."/data/".$real_name[0];
+			  }
+			//	if($i%10==1) echo "</tr><tr>";
+			?>
             <div class="existing-img-item">
-              <img src="/inc/assets/images/slide01.jpeg" alt="">
+              <img src="<?=$fileName?>" alt="">
               <div class="img-item-chk">
-                <input type="checkbox">
-                <p>1</p>
+                <input type="checkbox" name="img_num[]" value="<?=$cnt+1?>">
+                <p><?=$cnt+1?></p>
               </div>
             </div>
+			<?
+			  $cnt++;
+			}	
+			?>
           </div>
         </ul>
-      </div>
+<? } ?>
+	  </div>
 
       <div class="post-btn-box">
-        <button class="post-btn select-del-btn">선택삭제</button>
+<? if($wc_idx){ ?>
+        <button class="post-btn select-del-btn" type="button" onclick="image_alldel()">선택삭제</button>
+<? } ?>
         <div>
-          <button class="post-btn show-list-btn">
-            <a href="/menu02/workStatus_list.php">목록보기</a>
-          </button>
+          <button class="post-btn show-list-btn" type="button" onclick="location.href='/menu02/workStatus_list.php';">목록보기</button>
           <button class="post-btn register-btn">등록하기</button>
         </div>
       </div>
@@ -149,6 +249,5 @@
   </section>
 </main>
 
-<script src="/inc/js/car-slide.js"></script>
 <? include $_SERVER['DOCUMENT_ROOT']."/inc/footer.php"; ?>
 <? include $_SERVER['DOCUMENT_ROOT']."/inc/quick.php"; ?>
