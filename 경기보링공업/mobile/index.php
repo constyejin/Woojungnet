33eddabc91894a60e165dd27925497dae5092c9c
constyejin <lightup.jin@gmail.com>
2024-03-23 17:51:21 +0900

    📝 inclue mobile version footer

---
+++ 경기보링공업/mobile/index.php
@@ -0,0 +1,24 @@
+<? include $_SERVER['DOCUMENT_ROOT']."/mobile/inc/header.php"; ?>
+
+<section class="main-img">
+  <div class="main-img-box">
+    <img src="/inc/assets/images/slide03.jpeg" alt="main-image">
+  </div>
+
+  <div class="slide-fix-txt">
+    <div class="slide-fix-txt-main">
+      <p>자동차엔진&#40;보링&#41;전문기업</p>
+      <p>&#40;주&#41;<span>보링공업에서</span></p>
+      <p>안전한서비스를 보장합니다</p>
+    </div>
+  </div>
+</section>
+
+<div class="main-bottom-menu">
+  <ul>
+    <li><a href="/mobile/menu01/intro.php/#map-sec">오시는길</a></li>
+    <li><a href="/mobile/menu03/estimate.php">견적신청</a></li>
+  </ul>
+</div>
+
+<? include $_SERVER['DOCUMENT_ROOT']."/mobile/inc/footer.php"; ?>
