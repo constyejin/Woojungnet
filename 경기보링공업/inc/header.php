90ddf5453d8843b9a6e64e33f016d4bfe306f8f8
constyejin <lightup.jin@gmail.com>
2024-03-23 15:26:01 +0900

    📝 Change viewport width

--- 경기보링공업/inc/header.php
+++ 경기보링공업/inc/header.php
@@ -20,9 +20,9 @@ $web_config=sql_fetch("select * from web_config where idx=1 ");
 <html lang="ko">
 <head>
   <meta charset="UTF-8">
-  <!-- <meta name="viewport" content="width=1200"> -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>&#40;주&#41;경기보링공업</title>
+  <meta name="viewport" content="width:1200">
+  <title><?=$web_config[web_sitename]?></title>
+  <?=$web_config[web_meta]?>
   <link rel="stylesheet" href="/inc/styles/reset.css">
   <link rel="stylesheet" href="/inc/styles/header.css">
   <link rel="stylesheet" href="/inc/styles/footer.css">
