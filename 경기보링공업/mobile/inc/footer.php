5c65c3c76819c507cefea254b5791b55b02e5b1d
constyejin <lightup.jin@gmail.com>
2024-03-24 22:04:13 +0900

    📝 코드 정렬

--- 경기보링공업/mobile/inc/footer.php
+++ 경기보링공업/mobile/inc/footer.php
@@ -1,27 +1,27 @@
-  <footer class="global-footer">
-    <div class="global-footer-wrap">
-      <div class="footer-info">
-        <div class="footer-box">
-          <ul>
-            <li>전화:032-571-4117/ 010-4598-8690</li>
-            <li>팩스:032-571-4118</li>
-          </ul>
+<footer class="global-footer">
+  <div class="global-footer-wrap">
+    <div class="footer-info">
+      <div class="footer-box">
+        <ul>
+          <li>전화:032-571-4117/ 010-4598-8690</li>
+          <li>팩스:032-571-4118</li>
+        </ul>
 
-          <ul>
-            <li>상호:&#40;주&#41;경기보링공업</li>
-            <li>대표:이성삼</li>
-            <li>사업자번호:125-87-02875</li>
-          </ul>
+        <ul>
+          <li>상호:&#40;주&#41;경기보링공업</li>
+          <li>대표:이성삼</li>
+          <li>사업자번호:125-87-02875</li>
+        </ul>
 
-          <ul>
-            <li>주소:인천광역시 서구 거북로24번길22.2동&#40;석남동&#41;</li>
-            <li>이메일:obba623@hanmail.net</li>
-          </ul>
+        <ul>
+          <li>주소:인천광역시 서구 거북로24번길22.2동&#40;석남동&#41;</li>
+          <li>이메일:obba623@hanmail.net</li>
+        </ul>
 
-          <p class="copyright">
-            Copyright ⓒ (주)경기보링공업 all rights reserved
-          </p>
-        </div>
+        <p class="copyright">
+          Copyright ⓒ (주)경기보링공업 all rights reserved
+        </p>
       </div>
     </div>
-  </footer>
+  </div>
+</footer>
